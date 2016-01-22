d55a148eb43b857d7af6685b544c3516899e218c
hwclass <harderworking.class@gmail.com>
2016-01-22 17:55:50 +0200

    created the main file as index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var TelegramBot = require('node-telegram-bot-api'),
+		token = '150907634:AAGuwdo_1faKY-LS-2qudZ6NMG3TEEFDeQQ',
+		bot = new TelegramBot(token, {polling: true});
+
+var SaidWhat = (function (quotes) {
+
+	function getQuote (typedName, callback) {
+		var currentQuote;
+		for (var quoteIndex = 0, len = quotes.quotes.length; quoteIndex < len; quoteIndex++) {
+			if (quotes.quotes[quoteIndex]['name'] === typedName) {
+				var randomQuoteIndex = Math.floor((Math.random() * quotes.quotes[quoteIndex]['quotes'].length - 1) + 1);
+				currentQuote = quotes.quotes[quoteIndex]['quotes'][randomQuoteIndex];
+			}
+		}
+		callback(currentQuote);
+	}
+
+	return {
+		get : getQuote
+	}
+
+})(JSON.parse(JSON.stringify(require('./data/quotes.json'))));
+
+bot.onText(/\/saidwhat (.+)/, function (msg, match) {
+  var fromId = msg.from.id;
+  var resp = null; 
+  SaidWhat.get(match[1], function (quote) {
+  	resp = quote;
+  	bot.sendMessage(fromId, resp);
+  });
+});
